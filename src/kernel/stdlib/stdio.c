32ac66d9a7eefd7023c934b9a41a60b7e211c1db
nobody1902 <nobody1902@proton.me>
2026-04-06 22:32:12 +0200

    Switched to using heap allocations in stdio.

--- src/kernel/stdlib/stdio.c
+++ src/kernel/stdlib/stdio.c
@@ -2,23 +2,22 @@
 #include "float.h"
 #include "int.h"
 #include "keyboard/input.h"
+#include "mem/heap.h"
 #include "vga/print.h"
 #include <stdarg.h>
 #include <stdbool.h>
 
 void putc(char c) { print_char(c); }
+
 char inc() {
   bool shiftPressed = false;
   while (true) {
     keyCode_t key = scanKey();
-
     if (!isAscii(key)) {
-      if (key == LEFT_SHIFT_PRESSED) {
+      if (key == LEFT_SHIFT_PRESSED)
         shiftPressed = true;
-      } else if (key == LEFT_SHIFT_RELEASED) {
+      else if (key == LEFT_SHIFT_RELEASED)
         shiftPressed = false;
-      }
-
       continue;
     }
     return toAscii(key, shiftPressed);
@@ -26,10 +25,8 @@ char inc() {
 }
 
 void puts(char *s) {
-  while (*s) {
-    putc(*s);
-    s++;
-  }
+  while (*s)
+    putc(*s++);
 }
 
 void printf(const char *fmt, ...) {
@@ -39,72 +36,110 @@ void printf(const char *fmt, ...) {
   while (*fmt) {
     if (*fmt == '%') {
       fmt++;
-      if (*fmt == 'd' || *fmt == 'i') {
-        char buff[64];
+      char *buff = NULL;
+
+      switch (*fmt) {
+      case 'd':
+      case 'i':
+        buff = (char *)kmalloc(12);
+        if (!buff)
+          return;
         itoa(va_arg(args, int), buff, 10);
         puts(buff);
-      } else if (*fmt == 'u') {
-        char buff[64];
+        kfree(buff);
+        break;
+
+      case 'u':
+        buff = (char *)kmalloc(11);
+        if (!buff)
+          return;
         utoa(va_arg(args, unsigned int), buff, 10);
         puts(buff);
-      } else if (*fmt == 'f' || *fmt == 'F') {
-        char buff[64];
-        double f = va_arg(args, double);
-        ftos((float)f, buff);
-        puts(buff);
-      } else if (*fmt == 'x' || *fmt == 'X') {
-        char buff[64];
+        kfree(buff);
+        break;
+
+      case 'x':
+      case 'X':
+        buff = (char *)kmalloc(9);
+        if (!buff)
+          return;
         utoa(va_arg(args, unsigned int), buff, 16);
+        if (*fmt == 'X') {
+          for (char *p = buff; *p; p++)
+            if (*p >= 'a' && *p <= 'f')
+              *p -= 32;
+        }
         puts(buff);
-      } else if (*fmt == 'o') {
-        char buff[64];
+        kfree(buff);
+        break;
+
+      case 'o':
+        buff = (char *)kmalloc(12);
+        if (!buff)
+          return;
         utoa(va_arg(args, unsigned int), buff, 8);
         puts(buff);
-      } else if (*fmt == 's') {
-        puts(va_arg(args, char *));
-      } else if (*fmt == 'c') {
+        kfree(buff);
+        break;
+
+      case 'f':
+        buff = (char *)kmalloc(64);
+        if (!buff)
+          return;
+        ftos((float)va_arg(args, double), buff);
+        puts(buff);
+        kfree(buff);
+        break;
+
+      case 's': {
+        char *s = va_arg(args, char *);
+        if (!s)
+          s = "(null)";
+        puts(s);
+        break;
+      }
+
+      case 'c':
         putc((char)va_arg(args, int));
-      } else if (*fmt == '%') {
+        break;
+
+      case '%':
         putc('%');
+        break;
+
+      default:
+        putc('%');
+        putc(*fmt);
+        break;
       }
     } else {
       putc(*fmt);
     }
     fmt++;
   }
+
   va_end(args);
 }
-
 int strtoi_base(const char *str, int base, int *out_value) {
   int value = 0;
   bool negative = false;
   const char *p = str;
 
-  // Skip whitespace
   while (*p == ' ' || *p == '\t' || *p == '\n')
     p++;
 
-  // Optional sign for base 10 and 16
   if (base == 10 || base == 16) {
     if (*p == '-') {
       negative = true;
       p++;
-    } else if (*p == '+') {
+    } else if (*p == '+')
       p++;
-    }
   }
 
-  if (base == 16) {
-    // Optional "0x" or "0X" prefix
-    if (p[0] == '0' && (p[1] == 'x' || p[1] == 'X')) {
-      p += 2;
-    }
-  } else if (base == 8) {
-    // Optional "0" prefix
-    if (*p == '0') {
-      p++;
-    }
-  }
+  if (base == 16 && p[0] == '0' && (p[1] == 'x' || p[1] == 'X'))
+    p += 2;
+  else if (base == 8 && *p == '0')
+    p++;
 
   bool digit_found = false;
   while (*p) {
@@ -129,103 +164,138 @@ int strtoi_base(const char *str, int base, int *out_value) {
   }
 
   if (!digit_found)
-    return 0; // No digits parsed
+    return 0;
 
   *out_value = negative ? -value : value;
-  return 1; // success
+  return 1;
 }
 
-void read_string(char *buf, int max_len, bool ignore_whitespace) {
-  int i = 0;
-  char c;
+static char *read_string_dynamic(bool ignore_whitespace) {
+  int capacity = 16;
+  int len = 0;
+  char *buf = (char *)kmalloc(capacity);
+  if (!buf)
+    return NULL;
 
+  char c;
   do {
     c = inc();
   } while (c == ' ' || c == '\t' || c == '\n');
 
-  // Read until whitespace or max_len-1
-  while ((ignore_whitespace || (c != ' ' && c != '\t')) && c != '\n' &&
-         c != '\0' && i < max_len - 1) {
-    buf[i++] = c;
+  while (ignore_whitespace || (c != ' ' && c != '\t')) {
+    if (c == '\n' || c == '\0')
+      break;
+
+    if (len + 1 >= capacity) {
+      int new_cap = capacity * 2;
+      char *new_buf = (char *)kmalloc(new_cap);
+      if (!new_buf) {
+        kfree(buf);
+        return NULL;
+      }
+      for (int i = 0; i < len; i++)
+        new_buf[i] = buf[i];
+      kfree(buf);
+      buf = new_buf;
+      capacity = new_cap;
+    }
+
+    buf[len++] = c;
     c = inc();
   }
-  buf[i] = '\0';
+
+  buf[len] = '\0';
+  return buf;
 }
 
 int scanf(const char *fmt, ...) {
   va_list args;
   va_start(args, fmt);
-
   int assigned = 0;
-  char input_buf[64]; // buffer for reading strings and numbers
 
   while (*fmt) {
     if (*fmt == '%') {
       fmt++;
+      char *input_buf = NULL;
+
       if (*fmt == 'd' || *fmt == 'i') {
-        // Read string, convert base 10 signed int
-        read_string(input_buf, sizeof(input_buf), false);
+        input_buf = read_string_dynamic(false);
+        if (!input_buf)
+          break;
+
         int val;
         if (strtoi_base(input_buf, 10, &val)) {
           int *p = va_arg(args, int *);
           *p = val;
           assigned++;
         }
+        kfree(input_buf);
+
       } else if (*fmt == 'u') {
-        // Read string, convert base 10 unsigned int
-        read_string(input_buf, sizeof(input_buf), false);
+        input_buf = read_string_dynamic(false);
+        if (!input_buf)
+          break;
+
         int val;
         if (strtoi_base(input_buf, 10, &val)) {
           unsigned int *p = va_arg(args, unsigned int *);
           *p = (unsigned int)val;
           assigned++;
         }
+        kfree(input_buf);
+
       } else if (*fmt == 'x' || *fmt == 'X') {
-        // Read string, convert base 16 unsigned int
-        read_string(input_buf, sizeof(input_buf), false);
+        input_buf = read_string_dynamic(false);
+        if (!input_buf)
+          break;
+
         int val;
         if (strtoi_base(input_buf, 16, &val)) {
           unsigned int *p = va_arg(args, unsigned int *);
           *p = (unsigned int)val;
           assigned++;
         }
+        kfree(input_buf);
+
       } else if (*fmt == 'o') {
-        // Read string, convert base 8 unsigned int
-        read_string(input_buf, sizeof(input_buf), false);
+        input_buf = read_string_dynamic(false);
+        if (!input_buf)
+          break;
+
         int val;
         if (strtoi_base(input_buf, 8, &val)) {
           unsigned int *p = va_arg(args, unsigned int *);
           *p = (unsigned int)val;
           assigned++;
         }
+        kfree(input_buf);
+
       } else if (*fmt == 's') {
-        // Read string until whitespace
-        char *p = va_arg(args, char *);
-        read_string(p, 256, true); // assume caller allocated enough space
+        char **p = va_arg(args, char **);
+        char *str = read_string_dynamic(true);
+        if (!str)
+          break;
+        *p = str;
         assigned++;
+
       } else if (*fmt == 'c') {
-        // Read single char
         char *p = va_arg(args, char *);
         *p = inc();
         assigned++;
+
       } else if (*fmt == '%') {
-        // Literal '%', consume one char from input and compare
         char c = inc();
-        if (c != '%') {
-          // mismatch, stop parsing
+        if (c != '%')
           break;
-        }
-      } else {
-        // Unsupported format specifier, ignore or break
+
+      } else
         break;
-      }
+
       fmt++;
     } else {
-      // Match literal character from input
       char c = inc();
-      if (c != *fmt) {
+      if (c != *fmt)
         break;
-      }
       fmt++;
     }
   }
